2d87e53d73d586f560085f441b6b63b25f6ddb31
Ben Scott <ben@belfryimages.com.au>
2014-03-16 19:12:29 +1000

    Stubbed out Guard.For and Guard.Against

---
+++ src/Invariably/Guard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Invariably
+{
+    public static class Guard
+    {
+        public static void For(Action<bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Against(Action<bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
